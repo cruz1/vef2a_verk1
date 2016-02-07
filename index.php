51897e58b4b4c13658b3cd842cc51bfa56719314
Friðrik Njálsson <cruz1@users.noreply.github.com>
2016-02-07 23:01:55 +0000

    index

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<?php include_once("title.php") ?>
+</head>
+<body>
+	<div class="well"><h1>Njálsson</h1></div>
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-6 mynd">
+				<?php include_once("myndir.php") ?>
+
+			</div>
+		</div>
+		
+	</div>
+	<?php include_once("footer.php") ?>
+	
+</body>
+</html>
