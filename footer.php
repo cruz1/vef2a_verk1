25a670881c4e62e4e1f4cb923faafebfb96bc563
Friðrik Njálsson <cruz1@users.noreply.github.com>
2016-02-07 23:02:51 +0000

    footerinn

---
+++ footer.php
@@ -0,0 +1,8 @@
+
+	<nav class="navbar navbar-fixed-bottom">
+	  <div class="container-fluid">
+	    <div class="navbar">
+	      <?php echo "&copy; 2015 Friðrik Njálsson"  ?>
+	    </div>
+	  </div>
+	</nav>
